11c2aa83709696fd15adbb379989043fd14fd6ea
sudhankumar2810 <sudhankumar2810@gmail.com>
2026-06-23 16:00:12 +0530

    basic if validation is optimized using certain conditions

--- Day_3/MoneyWithdrawal.java
+++ Day_3/MoneyWithdrawal.java
@@ -10,26 +10,23 @@ public class MoneyWithdrawal {
         int balance = scanner.nextInt();
         System.out.print("Enter the amount to withdraw : ");
         int withdrawAmount = scanner.nextInt();
-
-        if(withdrawAmount>0)
-        {
-            if(withdrawAmount<=balance)
-            {
-                if(withdrawAmount%100==0)
-                {
-                    int currentBalance = balance-withdrawAmount;
-                    System.out.println("Withdrawal successful");
-                    System.out.println("Remaining balance : "+currentBalance);
-                }
-                else
-                    System.out.println("Amount should be multiple of 100");
-            }
-            else
-                System.out.println("Insufficient Balance");
+        if(balance>=0) {
+            if (withdrawAmount > 0) {
+                if (withdrawAmount <= balance) {
+                    if (withdrawAmount % 100 == 0) {
+                        int currentBalance = balance - withdrawAmount;
+                        System.out.println("Withdrawal successful");
+                        System.out.println("Remaining balance : " + currentBalance);
+                    } else
+                        System.out.println("Amount should be multiple of 100");
+                } else
+                    System.out.println("Insufficient Balance");
+            } else
+                System.out.println("Invalid amount. Enter valid amount");
         }
         else
-            System.out.println("Invalid amount. Enter valid amount");
-
+            System.out.println("Invalid account balance");
+        scanner.close();
     }
 
 }
